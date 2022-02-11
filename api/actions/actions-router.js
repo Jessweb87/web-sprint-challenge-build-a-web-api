c48451feb636d8045e55ba9d7389f7d301b05bde
Jessweb87 <jprosper87@gmail.com>
2022-02-11 13:22:10 -0500

    wip

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,21 @@
 // Write your "actions" router here!
+const express = require('express')
+const Actions = require('./actions-model')
+const router = express.Router()
+router.use(express.json())
+const { validateActionId, validateAction } = require('./actions-middlware')
+
+router.get('/', (req, res) => {
+    Actions.get()
+    .then((actions) => {
+        res.status(200).json(actions)
+    })
+    .catch((err) => {
+        res.status(500).json({
+            message: err.message,
+            customMessage: 'Actions get all error'
+        })
+    })
+})
+
+module.exports = router
